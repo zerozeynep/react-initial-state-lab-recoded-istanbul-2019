60b9856fef0e859455492f3e19b4bd4b47cc687f
zeynep gizem demirtaş <zeynep-demirtas@windowslive.com>
2020-03-03 12:42:12 +0000

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,31 @@
 // your Bomb code here!
+import React, { Component } from 'react'
+
+class Bomb extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      secondsLeft : props.initialCount
+         
+    }
+  }
+
+  render() {
+    if(this.state.secondsLeft===0){
+      return (
+        <div>Boom!</div>
+      )
+    }else {
+      return (
+        <div>
+         {this.state.secondsLeft} seconds left before I go boom!
+        </div>
+      )
+    }
+    
+  }
+}
+
+export default Bomb
+
--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,24 @@
 // your ImageSlider code here!
+import React, { Component } from 'react'
+
+class ImageSlider extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+          currentSlideIndex:0
+                 
+        } 
+    }
+
+    render() {
+        return (
+            <div>
+                I am on slide {this.state.currentSlideIndex}
+            </div>
+        )
+    }
+}
+
+export default ImageSlider
+
